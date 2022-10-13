3d5c2723beae1b6af2a0b4e78664c91f2d3d505e
Andres Vega <elb.andres8@gmail.com>
2022-10-12 22:17:35 -0500

    add statistics values

--- src/app/invoicing/statistics/statistics.component.ts
+++ src/app/invoicing/statistics/statistics.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { IIncomingOutcoming } from 'src/app/models/interfaces/incoming-outcomming.interface';
+import { InvoicingFacadeService } from 'src/app/shared/services/facades/invoicing-facade.service';
+
 @Component({
   selector: 'app-statistics',
   templateUrl: './statistics.component.html',
@@ -8,9 +11,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StatisticsComponent implements OnInit {
 
-  constructor() { }
+  incoming: number = 0;
+  outcoming: number = 0;
+
+  incomingTotal: number = 0;
+  outcomingTotal: number = 0;
+
+  constructor(
+    private readonly invoicingFacade: InvoicingFacadeService,
+  ) { }
 
   ngOnInit(): void {
+    this.invoicingFacade.items$.subscribe(items => this.makeCalcsStatistics(items));
+  }
+
+  makeCalcsStatistics(items: IIncomingOutcoming[]) {
+    let incoming = 0;
+    let outcoming = 0;
+    let incomingTotal = 0;
+    let outcomingTotal = 0;
+
+    for (const item of items) {
+      if (item.type === 'INCOMING') {
+        incoming++;
+        incomingTotal += Number(item.value);
+      } else {
+        outcoming++;
+        outcomingTotal += Number(item.value);
+      }
+    }
+
+    this.incoming = incoming;
+    this.outcoming = outcoming;
+    this.incomingTotal = incomingTotal;
+    this.outcomingTotal = outcomingTotal;
+  }
+
+  get difference() {
+    return this.incomingTotal - this.outcomingTotal;
   }
 
 }
